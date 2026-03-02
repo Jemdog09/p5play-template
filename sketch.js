1df916185e7b61742c1550d89e1d1e71bd9237d6
Jemdog09 <627jo44@canonslade.tbft.uk>
2026-03-02 19:14:48 +0000

    new file:   assets/5f35abbc5b7a741.gif 	new file:   assets/sonic sprite sheet.png 	new file:   assets/sr5zb748842ee2aws3.png 	new file:   assets/walk.gif 	modified:   index.html 	modified:   sketch.js 	new file:   sonic spritesheet/1fc07577ff93c643552e031a31696906.jpg 	new file:   sonic spritesheet/classic sonic spritesheet.png 	new file:   sonic spritesheet/crouch.png 	new file:   sonic spritesheet/idle.png 	new file:   sonic spritesheet/lookup.png 	new file:   sonic spritesheet/questKid.webp 	new file:   sonic spritesheet/rolling.png 	new file:   sonic spritesheet/running.png 	new file:   sonic spritesheet/sonic sprite sheet.png 	new file:   sonic spritesheet/sonic spritesheet.png 	new file:   sonic spritesheet/sonic_1_sprite_sheet_by_lildrizzy0331_dfheiu6-fullview.jpg 	new file:   sonic spritesheet/sonic_spritesheetv v2.png 	new file:   sonic spritesheet/sonic_testing_spritesheet.png 	new file:   sonic spritesheet/speedwalk.png 	new file:   sonic spritesheet/sprite_sheet.png 	new file:   sonic spritesheet/standing.png 	new file:   sonic spritesheet/up&down.png 	new file:   sonic spritesheet/walking.png 	new file:   walking animation/5f35abbc5b7a741 (1).gif 	new file:   walking animation/5f35abbc5b7a741 (1).png 	new file:   walking animation/_a_frm0,50.png 	new file:   walking animation/_a_frm1,50.png 	new file:   walking animation/_a_frm10,50.png 	new file:   walking animation/_a_frm11,50.png 	new file:   walking animation/_a_frm2,50.png 	new file:   walking animation/_a_frm3,50.png 	new file:   walking animation/_a_frm4,50.png 	new file:   walking animation/_a_frm5,50.png 	new file:   walking animation/_a_frm6,50.png 	new file:   walking animation/_a_frm7,50.png 	new file:   walking animation/_a_frm8,50.png 	new file:   walking animation/_a_frm9,50.png

--- sketch.js
+++ sketch.js
@@ -1,29 +1,182 @@
-createCanvas();
-world.gravity.y = 10;
+let player
+let ball
+let stats
+let floor
 
-let ball = new Sprite();
-ball.x = halfWidth - 200;
-ball.y = halfHeight - 200;
-ball.diameter = 50;
+function setup() {
 
-let groundA = new Sprite();
-groundA.x = halfWidth - 120;
-groundA.width = 200;
-groundA.rotation = 20;
-groundA.physics = STATIC;
+   new Canvas(700, 700, 'fullscreen');
+   walk = new Sprite();
+   player = new Sprite();
+   let block = new Sprite();
+   block.x = 510
+   block.y = 390
+   ball = new Sprite();
+   block.physics = STA
+   ball.physics = STA
+   player.physics = DYN
+   player.x = 500
+   player.y = 650
+   player.addCollider(60, 50);
+   player.debug = true;
+   player.mass = 35;
+   player.addAnis('sonic spritesheet/standing.png', {
+      stand: { row: 0, frames: 1 }
 
-let groundB = new Sprite();
-groundB.x = halfWidth + 120;
-groundB.width = 200;
-groundB.rotation = -20;
-groundB.physics = STATIC;
+   });
+
+   player.addAnis('sonic spritesheet/walking.png', {
+      walk: { row: 0, frames: 12, frameDelay: 6 },
+      speedwalk: { row: 0, frames: 12, frameDelay: 3 }
+   });
+
+   player.addAnis('sonic spritesheet/rolling.png', {
+      roll: { row: 0, frames: 8 }
+   });
+
+   player.addAnis('sonic spritesheet/idle.png', {
+      idle: { row: 0, frames: 5 }
+   });
+
+   player.addAnis('sonic spritesheet/running.png', {
+      run: { row: 0, frames: 4 }
+   });
+
+   player.addAnis('sonic spritesheet/lookup.png', {
+      lookup : { row: 0, frames: 3}
+
+      
+   });
+
+    player.addAnis('sonic spritesheet/crouch.png', {
+      crouch : { row: 0, frames: 2}
+
+      
+   });
+
+
+
+   stats = new Sprite(player.x - 300, player.y - 400, 100, 100, STA)
+
+
+   
+
+   environment();
+   player.rotationLock = true;
+   p5play.renderStats = true
+   
+   player.bounciness = 0
+
+}
+
+function designs() {
+   //player.img = 'assets/sr5zb748842ee2aws3.png'
+
+
+
+
+}
+
+
+
+function environment() {
+   world.gravity.y = 10
+   floor = new Sprite(860, 550, 1000000, 500, STA)
+   floor.color = 'green'
+   floor.offset.y = 250
+   floor.friction = 4.5
+   floor.bounciness = 0
+   ball.friction = floor.friction 
+}
+
+function grounded(){
+   if(player.colliding(floor)){
+      return true;
+   }
+   return false;
+}
+
+function basiccontrols() {
+
+   if (player.vel.x == 0 && grounded()) {
+      player.changeAni('stand')
+      stats.speed = player.vel.x
+   }
+
+   if (kb.pressing('left')) {
+      
+      player.changeAni('walk');
+      player.scale.x = -1
+      player.vel.x -= 0.3
+      stats.speed = player.speed
+      stats.direction = player.direction
+   } else if (kb.pressing('right')) {
+      
+      player.changeAni('walk');
+      player.scale.x = 1
+      player.vel.x += 0.3
+
+
+
+      stats.speed = player.speed
+      stats.direction = player.direction
+   }
+
+
+   if (kb.presses('space') && grounded()) {
+      player.vel.y = -10
+      player.changeAni('roll')
+   }
+
+
+   if (player.vel.x >= 5) {
+      player.changeAni('speedwalk')
+   }
+
+   if (player.speed >= 10 && player.vel.y === 0) {
+      player.changeAni('run')
+      player.drag = 0.27
+   } else if (player.vel.x < 10 && player.vel.x >= 5) {
+      player.changeAni('walk')
+   }
+
+    if (player.speed === 0 && kb.pressing('up')){
+      player.changeAni('lookup')
+    }
+
+        if (player.speed === 0 && kb.pressing('down')){
+      player.changeAni('crouch')
+    }
+
+
+    
+}
+
+
 
 function update() {
-	background('skyblue');
 
-	textAlign(CENTER);
-	textSize(20);
-	text('click to jump!', halfWidth, halfHeight - 100);
+   stats.text = player.speed.toFixed(0)
 
-	if (mouse.presses()) ball.vel.y = -5;
+   basiccontrols();
+   console.log(grounded())
+
+   background(20);
+
+
+    if (player.vel.x !== 0 && kb.pressing('down')){
+   player.changeAni('roll')
+}
+
+   if (player.vel.y !== 0) {
+      player.changeAni('roll')
+   }
+
+  
+
+}
+
+function drawFrame() {
+   camera.x = player.x
+   camera.y = player.y
 }
